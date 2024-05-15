3e378f361e35e06334e1d5412dcacd5366f69df2
sumaira <sumairafarooq8@gmail.co>
2024-05-15 12:48:47 +0530

    change

--- app.js
+++ app.js
@@ -11,21 +11,27 @@ app.get('/', function(req, res){
     }
     var filename = 'index.html';// to load our index.html
     res.sendFile(filename,options);
-})
-var users = 0;
+});
+var roomno = 1;
+var full = 0;
 /////**********server side connection/disconnection */
 io.on('connection', function(socket){
     console.log('A user connected');
-    users++;
-    io.sockets.emit('broadcast', {message: users + 'users connected'});
 
+    socket.join("room-"+roomno); //room created
+
+    io.sockets.in("room-"+roomno).emit('connectedRoom', "You Are Connected to room no. "+roomno); // event fired
+    full++;
+    if(full >= 2){
+        full = 0;
+        roomno++;
+    }
     socket.on('disconnect', function(){
-        console.log('A user disconnected');
-        users--;
-    io.sockets.emit('broadcast', {message: users + ' users connected'})
+        console.log('A user disconnected')
     });
 });
 ////////******************************************* */
+
 
 http.listen(3000, function(){ //3000 is port 
     console.log('Serverrrrr is ready on 3000')
